0683e8ea45126292a3d3f4619ebd4359d6e3a89b
Nikhil <email@nikhilchavan.com>
2021-08-31 19:00:44 +0530

    Fix typos in the plugin description

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Update Notifications.
- * Description: Display update notifications in the WordPress admi for sites
+ * Plugin Name: Update Notifications
+ * Description: Display Update Notifications for sites that have disabled file edits using constant `DISALLOW_FILE_MODS`
  * Version: 1.0.0
  * Author: Nikhil Chavan
  */
